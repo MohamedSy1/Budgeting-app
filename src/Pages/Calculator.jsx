e0cf26b61bbae2c7846f64fc56e402e8270f4731
MohamedSy1 <mohamed.sy@compscihigh.org>
2024-01-17 21:17:21 -0500

    attempted for better styling

--- src/Pages/Calculator.jsx
+++ src/Pages/Calculator.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Form from '../component/Form';
-import InflowOutflow from '../component/inflowoutflow';
+
 import NavBar from '../component/navbar';
 const InputBox = () => {
     const [entries, setEntries] = useState([]);
@@ -18,26 +18,21 @@ const InputBox = () => {
         <div>
             <NavBar />
             <Form onNewEntry={handleNewEntry} />
-            <InflowOutflow />
             <div className='border'>
-                <div className='flex justify-between text-center p-4 bg-gray-50 border border-gray-300 top-[0]'>
-                    <span className='flex-1 font-extrabold'>Category</span>
-                    <span className='flex-1 font-extrabold'>Description</span>
-                    <span className='flex-1 font-extrabold'>Amount</span>
+                <div className='flex flex-wrap overflow-y-auto h-[300px] p-4 border'> {/* Enable vertical scrolling and set a fixed height */}
+                    <ul className='w-full'> {/* Full width for the ul element */}
+                        {entries.map(entry => (
+                            <li key={entry.id} className='bg-white shadow-md rounded-lg m-2 p-4'> {/* Full width cards */}
+                                <span className='text-lg font-semibold'>{entry.category}</span>
+                                <p className='text-gray-600'>{entry.description}</p>
+                                <span className='mt-2 font-bold'>{entry.value}</span>
+                            </li>
+                        ))}
+                    </ul>
                 </div>
-                <ul>
-                    {entries.map(entry => (
-                        <li key={entry.id} className='flex justify-between text-center p-2 border-t'>
-                            
-                            <span className='flex-1'>{entry.category}</span>
-                            <span className='flex-1'>{entry.description}</span>
-                            <span className='flex-1'>{entry.value}</span>
-                        </li>
-                    ))}
-                </ul>
-                </div>
+            </div>
         </div>
     );
-};
+}
 
 export default InputBox;
---
+++ src/Pages/Results.jsx
@@ -0,0 +1,1 @@
+import InflowOutflow from '../component/inflowoutflow';
